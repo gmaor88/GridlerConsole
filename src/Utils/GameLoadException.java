096b2ba46add070c5928a441b258a4ae53b188c6
orkitay <shaool18@gmail.com>
2016-08-19 16:55:59 +0300

    load game from file

--- src/Utils/GameLoadException.java
+++ src/Utils/GameLoadException.java
@@ -6,11 +6,12 @@ package Utils;
 public class GameLoadException extends Exception {
     String m_ErrorMsg;
 
+    public GameLoadException(String i_ErrorMsg){
+        m_ErrorMsg = i_ErrorMsg;
+    }
+
     public String get_ErorMsg() {
         return m_ErrorMsg;
     }
 
-    public void set_ErorMsg(String i_ErrorMsg) {
-        m_ErrorMsg = i_ErrorMsg;
-    }
 }
--- src/Utils/GameLoader.java
+++ src/Utils/GameLoader.java
@@ -8,7 +8,7 @@ import jaxb.GameDescriptor;
  * Created by dan on 8/19/2016.
  */
 public class GameLoader {
-    public GameBoard load(GameDescriptor i_GameDescriptor){
+    public GameBoard load(GameDescriptor i_GameDescriptor) throws Exception{
         int columns, rows, blocks[], numberOfSlices, slicesId, numberOfBlackSquares;
         int columnIndex, rowIndex;
         GameBoard board;
@@ -18,10 +18,10 @@ public class GameLoader {
         numberOfSlices = i_GameDescriptor.getBoard().getDefinition().getSlices().getSlice().size();
         numberOfBlackSquares = i_GameDescriptor.getBoard().getSolution().getSquare().size();
         if(rows + columns != numberOfSlices){
-            throw ...
+            throw new GameLoadException("Invalid number of slices");
         }
         else if(rows*columns < numberOfBlackSquares){
-            throw ...
+            throw new GameLoadException("Invalid number of black squares");
         }
 
         board = new GameBoard(rows,columns);
@@ -30,13 +30,21 @@ public class GameLoader {
             blocks = getBlocks(i_GameDescriptor.getBoard().getDefinition().getSlices().getSlice().get(i).getBlocks());
             slicesId = i_GameDescriptor.getBoard().getDefinition().getSlices().getSlice().get(i).getId().intValue();
             if(i_GameDescriptor.getBoard().getDefinition().getSlices().getSlice().get(i).getOrientation() == "rows"){
+                if(getNumberOfBlackSquare(blocks) > columns){
+                    throw new GameLoadException("Invalid number of blocks in column "+ slicesId);
+                }
+
                 board.setHorizontalSlice(slicesId,blocks);
             }
             else if(i_GameDescriptor.getBoard().getDefinition().getSlices().getSlice().get(i).getOrientation() == "columns"){
+                if(getNumberOfBlackSquare(blocks) > rows){
+                    throw new GameLoadException("Invalid number of blocks in row "+ slicesId);
+                }
+
                 board.setVerticalSlice(slicesId, blocks);
             }
             else{
-                throw ...
+                throw new GameLoadException("Invalid orientation");
             }
         }
 
@@ -53,9 +61,25 @@ public class GameLoader {
         }
     }
 
-    private int[] getBlocks(String i_Blocks) {
-        int blocks[];
+    private int getNumberOfBlackSquare(int[] blocks) {
+        int sum = 0;
 
+        for(int i:blocks){
+            sum += i + 1;
+        }
+
+        return  sum - 1;
+    }
+
+    private int[] getBlocks(String i_Blocks) {
+        int blocks[], i = 0;
+        char intermidate[];
+
+        i_Blocks = i_Blocks.trim();
+        intermidate = i_Blocks.toCharArray();
+        for (char ch:intermidate){
+            if
+        }
         return blocks;
     }
 }
