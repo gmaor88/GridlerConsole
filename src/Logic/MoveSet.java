5abd9659b8d4ee7447d825c58f530b125eb1c955
gmaor88 <gmaor88@gmail.com>
2016-08-20 12:00:39 +0300

    MoveSet

--- src/Logic/MoveSet.java
+++ src/Logic/MoveSet.java
@@ -8,19 +8,41 @@ public class MoveSet {
     private Integer m_StartSquareColNumber;
     private Integer m_EndSquareRowNumber;
     private Integer m_EndSquareColNumber;
-    private Square.eSquareSign m_squareSign;
-    private String m_comment;
+    private Square.eSquareSign m_SquareSign;
+    private String m_Comment;
 
     public MoveSet(Integer i_StartSquareRowNumber, Integer i_StartSquareColNumber,
                    Integer i_EndSquareRowNumber, Integer i_EndSquareColNumber,
-                   Square.eSquareSign i_sign, String i_comment){
+                   Square.eSquareSign i_Sign, String i_Comment){
         m_StartSquareRowNumber = i_StartSquareRowNumber;
         m_StartSquareColNumber = i_StartSquareColNumber;
         m_EndSquareRowNumber = i_EndSquareRowNumber;
         m_EndSquareColNumber = i_EndSquareColNumber;
-        m_squareSign = i_sign;
-        m_comment = i_comment;
+        m_SquareSign = i_Sign;
+        m_Comment = i_Comment;
     }
 
-    
+    public Square.eSquareSign getSquareSign() {
+        return m_SquareSign;
+    }
+
+    public Integer getEndSquareColNumber() {
+        return m_EndSquareColNumber;
+    }
+
+    public Integer getEndSquareRowNumber() {
+        return m_EndSquareRowNumber;
+    }
+
+    public Integer getStartSquareColNumber() {
+        return m_StartSquareColNumber;
+    }
+
+    public Integer getStartSquareRowNumber() {
+        return m_StartSquareRowNumber;
+    }
+
+    public String getComment() {
+        return m_Comment;
+    }
 }
